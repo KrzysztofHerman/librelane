6ae6545ea79a3b81a42f079176ed3d604bb44aec
Krzysztof Herman <141824113+KrzysztofHerman@users.noreply.github.com>
2026-02-17 15:34:57 +0100

    Warn if KLAYOUT_FILLER_SCRIPT is unset
    Add warning for unset KLAYOUT_FILLER_SCRIPT configuration

--- librelane/steps/klayout.py
+++ librelane/steps/klayout.py
@@ -1031,6 +1031,14 @@ class Filler(KLayoutStep):
     def run(self, state_in: State, **kwargs) -> Tuple[ViewsUpdate, MetricsUpdate]:
         metrics_updates: MetricsUpdate = {}
         views_updates: ViewsUpdate = {}
+
+        
+        if not self.config["KLAYOUT_FILLER_SCRIPT"]:
+            self.warn(
+                f"KLAYOUT_FILLER_SCRIPT is unset. KLayout.Filler may not be supported for the {self.config['PDK']} PDK. This step will be skipped."
+            )
+            return views_updates, metrics_updates
+        
         if self.config["PDK"] in ["ihp-sg13g2", "ihp-sg13cmos5l"]:
             views_updates, metrics_updates = self.run_ihp_sg13g2(state_in, **kwargs)
         else:
